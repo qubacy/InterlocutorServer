6160ef02eadc3c98ac26400d3cca378ac7d24cd3
Leonid Artemev <leonidart14@gmail.com>
2023-10-30 01:02:03 +0700

    add test on equal topics from db

--- ws_server/storage/impl/sql/sqlite/topic_test.go
+++ ws_server/storage/impl/sql/sqlite/topic_test.go
@@ -3,6 +3,7 @@ package sqlite
 import (
 	"context"
 	"ilserver/domain"
+	"reflect"
 	"testing"
 )
 
@@ -23,7 +24,11 @@ func Test_InsertTopic(t *testing.T) {
 	}
 
 	for i := range topics {
-		storage.InsertTopic(context.Background(), topics[i])
+		_, err = storage.InsertTopic(context.Background(), topics[i])
+		if err != nil {
+			t.Errorf("Insert topic failed. Err: %v", err)
+			return
+		}
 	}
 
 	// ***
@@ -36,6 +41,45 @@ func Test_InsertTopic(t *testing.T) {
 
 	if num != len(topics) {
 		t.Errorf("Record count in the table is not equal %v", len(topics))
+		return
+	}
+}
+
+func Test_AllTopics(t *testing.T) {
+	storage, err := Instance()
+	if err != nil {
+		t.Error()
+		return
+	}
+
+	topicsForInsert := []domain.Topic{
+		{Lang: 1, Name: "test_name_1"},
+		{Lang: 2, Name: "test_name_2"},
+		{Lang: 3, Name: "test_name_3"},
+		{Lang: 4, Name: "test_name_4"},
+		{Lang: 5, Name: "test_name_5"},
+		//***
+	}
+
+	for i := range topicsForInsert {
+		_, err = storage.InsertTopic(context.Background(), topicsForInsert[i])
+		if err != nil {
+			t.Errorf("Insert topic failed. Err: %v", err)
+			return
+		}
+	}
+
+	// ***
+
+	readTopics, err := storage.AllTopics(context.Background())
+	if len(readTopics) != len(topicsForInsert) {
+		t.Errorf("Record count in the table is not equal %v", len(topicsForInsert))
+		return
+	}
+
+	// TODO:
+	if !reflect.DeepEqual(topicsForInsert, readTopics) {
+		t.Errorf("Topics are not equal to each other")
 		return
 	}
 }
